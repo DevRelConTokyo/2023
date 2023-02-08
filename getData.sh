6253edfcafc72142e89fe7a3cb73a08c6d88a066
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2023-02-08 14:38:36 +0900

    Add jobboard

--- getData.sh
+++ getData.sh
@@ -3,6 +3,7 @@ wget https://script.google.com/macros/s/$DEVREL_GOOGLE_ID/exec?name=organizers -
 wget https://script.google.com/macros/s/$DEVREL_GOOGLE_ID/exec?name=sessions -O _data/sessions.json
 wget https://script.google.com/macros/s/$DEVREL_GOOGLE_ID/exec?name=speakers -O _data/speakers.json
 wget https://script.google.com/macros/s/$DEVREL_GOOGLE_ID/exec?name=sponsors -O _data/sponsors.json
+wget https://script.google.com/macros/s/$DEVREL_GOOGLE_ID/exec?name=jobs -O _data/jobs.json
 
 ruby organizers.rb
 ruby speakers.rb

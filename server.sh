1661d829ecf07d2df29dd147d4af9078fa28df12
Atsushi Nakatsugawa <nakatsugawa@AtsushinoMac-mini.local>
2023-02-20 12:08:12 +0900

    add server.sh

--- server.sh
+++ server.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+if [ ! -e vendor/bundle ]; then
+	mkdir -p vendor/bundle
+fi
 docker run --rm \
   --volume="$PWD:/srv/jekyll:Z" \
   --volume="$PWD/vendor/bundle:/usr/local/bundle:Z" \
